38dfe3b54c972dd601a645a26cba549d6f923813
javrin <jawabiscuit@users.noreply.github.com>
2023-10-06 14:05:48 -0400

    Initial commit

---
+++ shell.bat
@@ -0,0 +1,2 @@
+@ echo off
+C:\Windows\System32\cmd.exe /k "cd /d "D:\dev\javrin\packages\msvc\14.37\vs_BuildTools\Common7\Tools\..\.." & "D:\dev\javrin\packages\msvc\14.37\vs_BuildTools\Common7\Tools\vsdevcmd.bat" "
