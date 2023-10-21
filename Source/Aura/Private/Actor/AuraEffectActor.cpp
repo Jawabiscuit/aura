69f139a587c13ed9a895fe73b901d78c15e38c78
javrin <jawabiscuit@users.noreply.github.com>
2023-10-21 08:51:37 -0400

    Strip down the effect actor

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -3,50 +3,15 @@
 #include "Actor/AuraEffectActor.h"
 
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/AuraAttributeSet.h"
-#include "Components/SphereComponent.h"
 
 AAuraEffectActor::AAuraEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
-}
-
-void AAuraEffectActor::OnOverlap(
-	UPrimitiveComponent* OverlappedComponent,
-	AActor*				 OtherActor,
-	UPrimitiveComponent* OtherComp,
-	int32				 OtherBodyIndex,
-	bool				 bFromSweep,
-	const FHitResult&	 SweepResult)
-{
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(
-			ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
-		ASCInterface->GetAbilitySystemComponent()->SetNumericAttributeBase(AuraAttributeSet->GetHealthAttribute(), AuraAttributeSet->GetHealth() + 25.f);
-		ASCInterface->GetAbilitySystemComponent()->SetNumericAttributeBase(AuraAttributeSet->GetManaAttribute(), AuraAttributeSet->GetMana() - 25.f);
-		Destroy();
-	}
-}
-
-void AAuraEffectActor::EndOverlap(
-	UPrimitiveComponent* OverlappedComponent,
-	AActor*				 OtherActor,
-	UPrimitiveComponent* OtherComp,
-	int32				 OtherBodyIndex)
-{
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
 void AAuraEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
 }
