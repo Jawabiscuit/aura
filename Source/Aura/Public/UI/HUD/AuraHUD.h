bbd71d11f2c3875dfa90cd5c2422db3848966071
javrin <jawabiscuit@users.noreply.github.com>
2023-11-17 15:51:06 -0500

    Aura ability system blueprint library

--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -11,6 +11,7 @@ class UAbilitySystemComponent;
 class UOverlayWidgetController;
 class UAuraUserWidget;
 struct FWidgetControllerParams;
+
 /**
  *
  */
@@ -19,14 +20,14 @@ class AURA_API AAuraHUD : public AHUD
 {
 	GENERATED_BODY()
 public:
-	UPROPERTY()
-	TObjectPtr<UAuraUserWidget> OverlayWidget;
-
 	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
 
 	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 
 private:
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
 
